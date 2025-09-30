094330268a86e5ceebb9389fce1991481272e46b
zzzkz <125101483+zzzkz@users.noreply.github.com>
2025-09-30 11:53:35 -0300

    Delete Pegarsources.bat

--- Pegarsources.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Baixar código-fonte do site e salvar em sources.txt na pasta Downloads silenciosamente
-set downloadPath=%USERPROFILE%\Downloads\sources.txt
-curl https://eventos.ifgoiano.edu.br/integra2025/ -o "%downloadPath%" >nul 2>&1
