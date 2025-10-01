de66377d2b739e123ecff7f874b6b1209d9fd996
zzzkz <125101483+zzzkz@users.noreply.github.com>
2025-10-01 18:41:33 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            BaixarCodigoFonte.main(new String[0]); // executa primeiro
-
-            ExtrairPalestrantes.main(new String[0]); // executa depois
+            BaixarCodigoFonte.main(new String[0]);
+            ExtrairPalestrantes.main(new String[0]);
+            ExtrairImagens.main(new String[0];
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
